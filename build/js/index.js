ce5ca8f8d8aa13d510e657b7381584f841db2a26
safwan <safwan.p@talrop.com>
2023-03-03 09:20:39 +0530

    httpRequest interface added

--- build/js/index.js
+++ build/js/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("project initialized");
+const getRequest = (req) => {
+    console.log(req.user.email);
+};
+const request = {
+    user: {
+        username: "safwan"
+    },
+    isAuthenticated: false,
+    data: {}
+};
+getRequest(request);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("project initialized")
+interface User {
+    username: string,
+    pk ?: string,
+    id ?: string,
+    first_name ?: string,
+    email ?: string,
+}
+
+interface HttpRequest {
+    user: User,
+    isAuthenticated: boolean,
+    data?: object,
+}
+
+const getRequest = (req: HttpRequest)=>{
+    console.log(req.user.email)
+}
+
+const request = {
+    user: {
+        username: "safwan"
+    },
+    isAuthenticated: false,
+    data: {}
+}
+
+getRequest(request)
